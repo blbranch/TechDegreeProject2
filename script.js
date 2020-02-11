4281ddba9582c3651155705d1a44f9181ab45fc6
Branch <48926382+blbranch@users.noreply.github.com>
2020-02-11 14:16:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+//global variables for max students per page & student list from HTML file
 const pageDisplayLimit = 10;
 const studentList = document.getElementsByClassName('student-list')[0].getElementsByTagName('LI')
 
 
 //Show Page Function //
 const showPage = (list, page) => {
+   /*determines the starting and ending index for the student list.  
+   If students are out of index they are hidden else they are displayed */
    let startIndex = (page * pageDisplayLimit) - pageDisplayLimit;
    let endIndex = page * pageDisplayLimit;
          
@@ -18,35 +21,39 @@ const showPage = (list, page) => {
    }
 }
 
+//call function on page load and set it to display page 1
 showPage(studentList, 1)
-
-
 
 
 //Append Page Links Function
 const appendPageLinks = (list) => {
-   //const pageCount = Math.ceil(liList.length / pageDisplayLimit)
+   //determine how many nav links based on passed list length and page display limit
    const pageCount = Math.ceil(list.length / pageDisplayLimit)
 
+   //remove any prior nav links before creating new ones
    let previousPages = document.getElementsByClassName('pagination');
    while(previousPages.length > 0) {
       previousPages[0].parentNode.removeChild(previousPages[0]);
    }
    
+   //create div elements and add pagination class
    let div = document.createElement('DIV');
    div.className = 'pagination';
    
+   //create ul element
    let page = document.getElementsByClassName('page')[0];
-   
    let ul = document.createElement('UL')
 
+   //create list element and anchor tags equal to pagecount
    for (let i = 0; i < pageCount; i++) {
       let li = document.createElement('LI');
       let anchor = document.createElement('A');
       anchor.textContent = i+1;
-      //adding a nav link class name so function below doesn't clear active state from non nav-link buttons
+      anchor.href = '#';
+      
       anchor.classList.add('nav-link')
-            
+
+      //add event listener to each nav link button            
       anchor.addEventListener("click", (e) => {
          e.preventDefault();
          //find active nav link anchor tags
@@ -56,25 +63,33 @@ const appendPageLinks = (list) => {
             activeLinks[0].classList.remove('active')
          }
          
-         //set active class to clicked nav link button
+         //set active class to last clicked nav link button
          e.target.className += ' active';
-         //call showPage function with student list and clicked link's text content as parameters
-         showPage(list, e.target.textContent);
-                  
+         //call showPage function with list and page number as parameters
+         showPage(list, e.target.textContent);                  
       })
-         
+      
+      //append anchor tags to the list element and the list elements to the ul
       li.appendChild(anchor);
       ul.appendChild(li);
    }
    
+   //append the ul to the div element and the finished div to the page
    div.appendChild(ul);
    page.appendChild(div);
+
+   //on page load and after searching set the first nav link to active
+   let firstNavButton = document.getElementsByClassName('nav-link')[0]
+   firstNavButton.className += ' active'
 }
 
-//call at page load
+//call at page load with the entire student list from HTML file
 appendPageLinks(studentList)
 
+//create search bar function
 const addSearchBar = () => {
+
+   //create a div  for search bar and append input and buttons to it
 
    let div = document.createElement('DIV');
    div.className = 'student-search';
@@ -90,55 +105,79 @@ const addSearchBar = () => {
    div.appendChild(input);
    div.appendChild(button);
    
+   //add input and buttons to page header
    let header = document.getElementsByClassName('page-header cf')[0];
-   header.appendChild(div);
-      
+   header.appendChild(div);      
 }
 
+//call at page load
 addSearchBar();
 
 const searchBar = document.querySelector('#searchBar');
 const searchButton = document.querySelector('#searchButton');
 
+/*add keyup event listener that calls the searchinput function 
+  with the input string and the student list*/
 searchBar.addEventListener('keyup', (e) => {
    searchInput(e.target.value, studentList)
 })
 
-function searchInput(searchInput, list) {   
+/*add click event in case user copies and pastes versus keys in.  
+   Event calls the searchinput function with the input string and the student list*/
+searchButton.addEventListener('click', (e) => {
+   e.preventDefault();
+   searchInput(searchBar.value, studentList);
+})
+
+/* create a no results found message and set its display to none.  
+   Append it to the page element*/
+const page = document.getElementsByClassName('page')[0];
+const noResults = document.createElement('H3');
+noResults.textContent = 'No students were found.  Please try again.';
+noResults.style.display = 'none'
+page.appendChild(noResults)
+
+/*Search input function*/
+function searchInput(input, list) {   
+
+   //clear any prior no matches found message
+   noResults.style.display = 'none'
 
    let matches = [];
    
    for (let i = 0; i < list.length; i++) {
       let currentStudent = list[i];
       let currentStudentName = currentStudent.querySelector('.student-details > h3').textContent;
-      
-      if (searchInput.length !== 0 && currentStudentName.toLowerCase().includes(searchInput.toLowerCase())) {
-         currentStudent.style.display = 'inherit';
+      /*if there is at least one character in the search field 
+        and a student's name includes that character or string then push to matches array. */        
+      if (input.length !== 0 && currentStudentName.toLowerCase().includes(input.toLowerCase())) {
          matches.push(currentStudent);         
 
+      //otherwise set the display to none
       } else {
          currentStudent.style.display = 'none';
        }       
+   }
+   /*if the search field is empty then call show page and append page with the entire student list 
+      and show the first page of students */    
+   if (input.length === 0) {
+      appendPageLinks(studentList);
+      showPage(studentList, 1);
+   }
 
-      }
+   /*if there is at least one match in the matches array then call appendPageLinks 
+     and showPage with the matches array.  Display first page*/
+   if (matches.length > 0) {
+      appendPageLinks(matches)
+      showPage(matches, 1)
+   }
+
+   /*if there are no matches in the matches array and there is at least one character in the searh field 
+     display the no results message and call appendPageLinks with an empty array*/
+
+   if (matches.length === 0 && input.length !== 0) {
+      noResults.style.display = 'inherit';         
       
-      if (searchInput.length === 0) {
-         appendPageLinks(studentList);
-         showPage(studentList, 1);
-      }
-
-      if (matches.length > 0) {
-         appendPageLinks(matches)
-         showPage(matches, 1)
-      }
-
-      if (matches.length === 0 && searchInput.length !== 0) {
-         alert('no matches found')
-         //TODO add logic here to create Inner HTML that no students were found.  Have to delete this at the beginning of this function
-         appendPageLinks([])
-      }
-   
+      appendPageLinks([])
+   }
 }
-
-
-
